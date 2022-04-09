44f4d8d7dfaa18b3764c484155c6fe85d830df69
murhussain <husseinmurashi@yahoo.com>
2022-04-09 15:38:53 +0200

    Introduction  to model serializer

--- movie/api/serializers.py
+++ movie/api/serializers.py
@@ -5,23 +5,12 @@ from rest_framework import serializers
 from movie.models import Movie
 
 
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
-    description = serializers.CharField()
-    active = serializers.BooleanField()
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = "__all__"
 
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
-        return instance
-
-    #  Object-level validation
+    # Object-level validation
     def Validate(self, data):
         if data['name'] == data['description']:
             raise serializers.ValidationError("The Name should not look the same as description")
@@ -35,3 +24,32 @@ class MovieSerializer(serializers.Serializer):
         else:
             return value
 
+# class MovieSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     name = serializers.CharField()
+#     description = serializers.CharField()
+#     active = serializers.BooleanField()
+#
+#     def create(self, validated_data):
+#         return Movie.objects.create(**validated_data)
+#
+#     def update(self, instance, validated_data):
+#         instance.name = validated_data.get('name', instance.name)
+#         instance.description = validated_data.get('description', instance.description)
+#         instance.active = validated_data.get('active', instance.active)
+#         instance.save()
+#         return instance
+#
+#     #  Object-level validation
+#     def Validate(self, data):
+#         if data['name'] == data['description']:
+#             raise serializers.ValidationError("The Name should not look the same as description")
+#         else:
+#             return data
+#
+#     #  Field-level validation
+#     def validate_name(self, value):
+#         if len(value) < 3:
+#             raise serializers.ValidationError("Name is Too Short")
+#         else:
+#             return value
